af28f3a16725ba6fabf9c3a18e1030bd1d05eaff
abaugus <abhishekbind2013@gmail.com>
2016-06-02 14:43:38 +0530

    on track15

--- csvToArray.js
+++ csvToArray.js
@@ -168,10 +168,10 @@ function Distance(p1, p2) {
 }
 
 function checkDeals() {
-    for (var i = 1; i <= data.length - 2; i++) {
-        var temp = String(data[i][0] + " " +data[i][4]);
+    for (var i = 2; i <= data.length - 2; i++) {
+        var temp = String(data[i][4]);
         //window.alert(temp);
-        temp = getAutoSuggestions(String(temp));
+        temp = getAutoSuggestions(temp);
         window.alert(temp);
         var ret = codeAddress(temp);
         //window.alert(ret);
