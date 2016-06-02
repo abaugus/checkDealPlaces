d391d39ca002617489e0ed91cb28ce5508926f2b
abaugus <abhishekbind2013@gmail.com>
2016-06-02 11:13:54 +0530

    validating1

--- csvToArray.js
+++ csvToArray.js
@@ -134,8 +134,8 @@ function checkDeals() {
     var data = CSVToArray(String(value));
     for (var i = 1; i <= data.length - 2; i++) {
         var temp = String(data[i][0] + data[i][4] + data[i][5]);
-        //console.log(data[i][0]+ " "+ data[i][1]);
-        temp = initialize(temp);
+        window.alert(data[i][0]+ " " + data[i][4]+ " " +data[i][5]);
+        /*temp = initialize(temp);
         var ret = codeAddress(temp);
         var p1 = new google.maps.LatLng(data[i][8], data[i][9]);
         var p2 = new google.maps.LatLng(ret[0], ret[1]);
@@ -143,8 +143,8 @@ function checkDeals() {
             window.alert("True");
         } else {
             window.alert("False");
-        }
+        }*/
     }
 }
 
-checkDeals();
+//checkDeals();
