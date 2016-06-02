23cc32f6753b6416c299e42e44d312fe0bff2023
abaugus <abhishekbind2013@gmail.com>
2016-06-02 12:42:54 +0530

    validating30

--- csvToArray.js
+++ csvToArray.js
@@ -1,66 +1,35 @@
 function CSVToArray(strData, strDelimiter) {
-    // Check to see if the delimiter is defined. If not,
-    // then default to comma.
     strDelimiter = (strDelimiter || ",");
-
-    // Create a regular expression to parse the CSV values.
     var objPattern = new RegExp(
         (
-            // Delimiters.
             "(\\" + strDelimiter + "|\\r?\\n|\\r|^)" +
 
-            // Quoted fields.
             "(?:\"([^\"]*(?:\"\"[^\"]*)*)\"|" +
 
-            // Standard fields.
             "([^\"\\" + strDelimiter + "\\r\\n]*))"
         ),
         "gi"
     );
 
-
-    // Create an array to hold our data. Give the array
-    // a default empty first row.
     var arrData = [
         []
     ];
 
-    // Create an array to hold our individual pattern
-    // matching groups.
     var arrMatches = null;
-
-
-    // Keep looping over the regular expression matches
-    // until we can no longer find a match.
+    
     while (arrMatches = objPattern.exec(strData)) {
 
-        // Get the delimiter that was found.
         var strMatchedDelimiter = arrMatches[1];
-
-        // Check to see if the given delimiter has a length
-        // (is not the start of string) and if it matches
-        // field delimiter. If id does not, then we know
-        // that this delimiter is a row delimiter.
         if (
             strMatchedDelimiter.length &&
             strMatchedDelimiter !== strDelimiter
         ) {
 
-            // Since we have reached a new row of data,
-            // add an empty row to our data array.
             arrData.push([]);
-
         }
 
         var strMatchedValue;
-
-        // Now that we have our delimiter out of the way,
-        // let's check to see which kind of value we
-        // captured (quoted or unquoted).
         if (arrMatches[2]) {
-
-            // We found a quoted value. When we capture
-            // this value, unescape any double quotes.
             strMatchedValue = arrMatches[2].replace(
                 new RegExp("\"\"", "g"),
                 "\""
@@ -68,58 +37,14 @@ function CSVToArray(strData, strDelimiter) {
 
         } else {
 
-            // We found a non-quoted value.
             strMatchedValue = arrMatches[3];
 
         }
-        // Now that we have our value string, let's add
-        // it to the data array.
         arrData[arrData.length - 1].push(strMatchedValue);
     }
-
-    // Return the parsed data.
     return (arrData);
 }
-/*
-function initialize() {
-    var address = (document.getElementById('my-address'));
-    var autocomplete = new google.maps.places.Autocomplete(address);
-    autocomplete.setTypes(['geocode']);
-    google.maps.event.addListener(autocomplete, 'place_changed', function() {
-        var place = autocomplete.getPlace();
-        if (!place.geometry) {
-            return;
-        }
 
-        var address = '';
-        if (place.address_components) {
-            address = [
-                (place.address_components[0] && place.address_components[0].short_name || ''),
-                (place.address_components[1] && place.address_components[1].short_name || ''),
-                (place.address_components[2] && place.address_components[2].short_name || '')
-            ].join(' ');
-        }
-    });
-}
-
-function codeAddress() {
-    geocoder = new google.maps.Geocoder();
-    var address = document.getElementById("my-address").value;
-    var ret = [];
-    geocoder.geocode({
-        'address': address
-    }, function(results, status) {
-        if (status == google.maps.GeocoderStatus.OK) {
-            alert("Latitude: " + results[0].geometry.location.lat() + "Longitude: " + results[0].geometry.location.lng());
-            //ret[0] = results[0].geometry.location.lat();
-            //ret[1] = results[0].geometry.location.lng();
-        } else {
-            alert("Geocode was not successful for the following reason: " + status);
-        }
-    });
-    //return ret;
-}
-*/
 function getAutoSuggestions(address) {
         //var address = (document.getElementById('my-address'));
         var autocomplete = new google.maps.places.Autocomplete(address);
@@ -140,7 +65,7 @@ function getAutoSuggestions(address) {
         }
       });
 }
- function initialize(address) {
+function initialize(address) {
         //var address = (document.getElementById('my-address'));
         var autocomplete = new google.maps.places.Autocomplete(address);
         autocomplete.setTypes(['geocode']);
@@ -168,18 +93,16 @@ function codeAddress(address) {
         'address': address
     }, function(results, status) {
         if (status == google.maps.GeocoderStatus.OK) {
-            alert("Latitude: " + results[0].geometry.location.lat() + "Longitude: " + results[0].geometry.location.lng());
+            window.alert("Latitude: " + results[0].geometry.location.lat() + "Longitude: " + results[0].geometry.location.lng());
             ret[0] = results[0].geometry.location.lat();
             ret[1] = results[0].geometry.location.lng();
         } else {
-            alert("Geocode was not successful for the following reason: " + status);
+            window.alert("Geocode was not successful for the following reason: " + status);
         }
     });
     return ret;
 }
-/* 
-    Shows the distance between two LatLng in Metre
-*/
+
 function Distance(p1, p2) {
     var dist = google.maps.geometry.spherical.computeDistanceBetween(p1, p2) / 100000;
     return dist;
@@ -192,7 +115,7 @@ function checkDeals() {
         //temp = getAutoSuggestions(temp);
         window.alert(temp);
         var ret = codeAddress(temp);
-        window.alert(ret);
+        //window.alert(ret);
         if(ret.length>0)
         {
             var p1 = new google.maps.LatLng(data[i][8], data[i][9]);
